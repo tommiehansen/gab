1e172dbd07eca2fe375babeb0459cb15a09faf9f
tommiehansen <tommiehansen@gmail.com>
2018-04-04 16:07:06 +0200

    Improve error message

--- system/runner.php
+++ system/runner.php
@@ -235,9 +235,10 @@ $timer_end = timer_end($timer_start); // returns seconds
 if( $curl->status !== 200 )
 {
 	$str = "Runner.php ERROR: Running config via curl_post() did not return data.\n";
+	if( $curl->status == 0 ) $curl->status = '0 - No data';
 	$str .= "url: $url | curl status: " . $curl->status . "\n";
-	$str .= "Gekko conf:\n";
-	$str .= print_r($gconf);
+	#$str .= "Gekko conf:\n";
+	#$str .= print_r($gconf);
 	die();
 }
 
